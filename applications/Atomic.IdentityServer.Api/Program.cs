37ae7c780a5da52674a6b60f22e7bdff9aa70a4b
taujiong <taujiong@outlook.com>
2021-11-25 10:21:36 +0800

    feat(IdentityServer.Api): add support of EntityFrameworkCore

--- applications/Atomic.IdentityServer.Api/Program.cs
+++ applications/Atomic.IdentityServer.Api/Program.cs
@@ -1,3 +1,6 @@
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region Add services to the container
@@ -5,6 +8,26 @@ var builder = WebApplication.CreateBuilder(args);
 var mvcBuilder = builder.Services.AddControllers();
 builder.AddAtomicLocalization(mvcBuilder);
 builder.ConfigureApiController();
+
+var connectionString = builder.Configuration.GetConnectionString("IdentityServer");
+var migrationAssembly = typeof(Program).Assembly.GetName().FullName;
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore<ConfigurationDbContext>(options =>
+    {
+        options.ConfigureDbContext = b => b.UseNpgsql(connectionString, sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(15), null);
+            sqlOptions.MigrationsAssembly(migrationAssembly);
+        });
+    })
+    .AddOperationalStore<PersistedGrantDbContext>(options =>
+    {
+        options.ConfigureDbContext = b => b.UseNpgsql(connectionString, sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(15), null);
+            sqlOptions.MigrationsAssembly(migrationAssembly);
+        });
+    });
 
 #endregion
 
